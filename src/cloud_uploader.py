abfecbe34001fae1e9a32288e1dcfd47245b9d0c
Elios <94278470+Elkhn@users.noreply.github.com>
2022-04-19 22:35:58 +0400

    some fixes

--- src/cloud_uploader.py
+++ src/cloud_uploader.py
@@ -2,6 +2,8 @@ import os
 import yadisk
 # pip install yadisk
 
+TOKEN = "AQAAAABf0mDbAAfRILWUrGyexE5BjRS_rtLyAWc"
+
 def upload_files(ynd_api, from_dir, to_dir):
     parent_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
     from_dir = os.path.join(parent_dir, from_dir)
@@ -10,17 +12,17 @@ def upload_files(ynd_api, from_dir, to_dir):
     try:
         ynd_api.mkdir(to_dir)
     except yadisk.exceptions.PathExistsError:
-        pass
+        print(f'The folder {to_dir} already exists.')
     
     for file in docs_names:
         file_path = os.path.join(from_dir, file)
         try:
             ynd_api.upload(file_path, to_dir + '/' + file)
         except yadisk.exceptions.PathExistsError:
-            pass
+            print(f'The file {file} already exists.')
 
 if __name__ == '__main__':
-    ynd_api = yadisk.YaDisk(token="AQAAAABf0mDbAAfRILWUrGyexE5BjRS_rtLyAWc")
+    ynd_api = yadisk.YaDisk(token=TOKEN)
     to_dir = "database"
     from_dir = "data_1"
     upload_files(ynd_api, from_dir, to_dir)
